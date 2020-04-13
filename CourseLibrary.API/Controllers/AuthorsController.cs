9bc6fa6708fbbcb48bce91db58e754a1aacddd83
Marcin Wandel <wandelmarcin@gmail.com>
2020-04-13 11:39:54 +0200

    Add whole api project

---
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using CourseLibrary.API.Helpers;
+using CourseLibrary.API.Models;
+using CourseLibrary.API.Parameters;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("/api/authors")]
+    public class AuthorsController:ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+        private readonly IMapper _mapper;
+
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository, IMapper mapper)
+        {
+            _courseLibraryRepository = courseLibraryRepository ??
+                throw new ArgumentNullException(nameof(courseLibraryRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AuthorDto>> GetAuthors([FromQuery] AuthorParametes authorParametes)
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors(authorParametes);
+
+            var authors = _mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepo);
+
+            return Ok(authors);
+        }
+
+        [HttpGet("{authorId}")]
+        public IActionResult GetAuthor(Guid authorId)
+        {
+            var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId);
+            
+            if (authorFromRepo == null)
+            {
+                return NotFound(authorId);
+            }
+            
+            var author = _mapper.Map<AuthorDto>(authorFromRepo);
+            return Ok(author);
+        }
+    }
+}
